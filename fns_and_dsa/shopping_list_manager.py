699b29f0322cd6c5a9b101390b663ba13da5684e
DannyIshimwe <ishimwedanny62@gmail.com>
2024-07-05 02:08:56 +0200

    the perfomance of arithmetic operation

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,14 +1,9 @@
-def display_menu():
-    print("\nShopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
-    print("4. Exit")
+
 
 def main():
     shopping_list = []
     while True:
-        display_menu()
+        
         choice = input("Enter your choice: ")
 
         if choice == '1':
