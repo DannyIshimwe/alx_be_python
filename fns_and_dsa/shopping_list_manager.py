fadaf39695148e9b3b628be8d8fddd441fc00259
DannyIshimwe <ishimwedanny62@gmail.com>
2024-07-05 12:25:42 +0200

    shopping project

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -4,28 +4,6 @@ def display_menu():
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-
-def add_item(shopping_list):
-    item = input("Enter item to add: ")
-    shopping_list.append(item)
-    print(f"{item} added to the shopping list.")
-
-def remove_item(shopping_list):
-    item = input("Enter item to remove: ")
-    if item in shopping_list:
-        shopping_list.remove(item)
-        print(f"{item} removed from the shopping list.")
-    else:
-        print(f"{item} not found in the shopping list.")
-
-def view_list(shopping_list):
-    if shopping_list:
-        print("Shopping List:")
-        for item in shopping_list:
-            print(f"- {item}")
-    else:
-        print("Shopping List is empty.")
-
 def main():
     shopping_list = []
     while True:
@@ -33,11 +11,23 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            add_item(shopping_list)
+            item = input("Enter the item to add: ")
+            shopping_list.append(item)
+            print(f"{item} has been added to the shopping list.")
         elif choice == '2':
-            remove_item(shopping_list)
+            item = input("Enter the item to remove: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} has been removed from the shopping list.")
+            else:
+                print(f"{item} not found in the shopping list.")
         elif choice == '3':
-            view_list(shopping_list)
+            if shopping_list:
+                print("Current Shopping List:")
+                for index, item in enumerate(shopping_list, start=1):
+                    print(f"{index}. {item}")
+            else:
+                print("The shopping list is empty.")
         elif choice == '4':
             print("Goodbye!")
             break
